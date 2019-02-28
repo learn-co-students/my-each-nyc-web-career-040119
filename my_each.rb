07e519a884f499e76e4abf9aed95df305edd0e5c
authentic-actor-8801 <xaviernational@aol.com>
2019-02-28 07:00:52 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array = [[1, 2, 3, 4]) # put argument(s) here
   # code here
+  i = 0
+  while 1 < array.length
+  	yield(array[i])
+  	puts i
+  end
+array
 end
